282b68c87f99a973f74b1fc1cfe63c82f7545867
Fei Yang <me@feiyang.moe>
2022-08-21 19:10:36 +0800

    feat(route): Add APNews hub feed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.0.0/mod.ts";
 
 import { PORT } from "./utils/consts.js";
+import { hub as apnewsHub } from "./routes/apnews.js";
 
 const router = new Router();
 router.get("/", (ctx) => {
   ctx.response.body = "Hello world!";
 });
+router.get("/apnews/hub/:hub", apnewsHub);
 
 const app = new Application();
 app.use(router.routes());
---
+++ src/routes/apnews.js
@@ -0,0 +1,61 @@
+import { fetchX } from "../utils/network.js";
+import { add, create, parse } from "../utils/feed.js";
+import { CONTENT_TYPES, ERRORS, LIMIT } from "../utils/consts.js";
+
+const HUB_NAME_REGEX = /^[0-9a-z-]+$/;
+
+async function hub(ctx, next) {
+  let response;
+  if (ctx.params.hub.match(HUB_NAME_REGEX) !== null) {
+    try {
+      response = await (await fetchX(
+        `https://storage.googleapis.com/afs-prod/feeds/${ctx.params.hub}.json.gz`,
+      )).json();
+    } catch (_) {
+      ctx.response.body = ERRORS["FAILED_UPSTREAM"];
+      ctx.response.status = 500;
+      return;
+    }
+
+    const feed = create(
+      ctx.request.url.href,
+      `AP News - ${response["tagObjs"][0]["name"]}`,
+      "AP News",
+      "https://apnews.com/favicon.ico",
+    );
+
+    let author;
+    for (let i = 0; i < response["cards"].length && i < LIMIT; i++) {
+      if (response["cards"][i]["cardType"] === "Wire Story") {
+        if (response["cards"][i]["contents"][0]["bylines"] !== null) {
+          author = response["cards"][i]["contents"][0]["bylines"].substr(3);
+        } else {
+          author = "AP News";
+        }
+
+        add(
+          feed,
+          `https://apnews.com/article/${
+            response["cards"][i]["contents"][0]["canonicalUrl"]
+          }-${response["cards"][i]["contents"][0]["shortId"]}`,
+          response["cards"][i]["contents"][0]["headline"],
+          new Date(response["cards"][i]["contents"][0]["published"])
+            .toISOString(),
+          author,
+          response["cards"][i]["contents"][0]["storyHTML"],
+          response["cards"][i]["contents"][0]["firstWords"],
+        );
+      }
+    }
+
+    ctx.response.body = parse(feed);
+    ctx.response.headers.set("content-type", CONTENT_TYPES.ATOM);
+    return;
+  } else {
+    ctx.response.body = ERRORS["INVALID_REQUEST"];
+    ctx.response.status = 400;
+    return;
+  }
+}
+
+export { hub };
