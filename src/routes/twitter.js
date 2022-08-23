ec0dc829a69930159c2d5a6abd7f8c9a7ec5b751
Fei Yang <me@feiyang.moe>
2022-08-23 20:43:48 +0800

    refactor(route): Use search params to add params instead of validate them.

--- src/routes/twitter.js
+++ src/routes/twitter.js
@@ -2,8 +2,6 @@ import { fetchX } from "../utils/network.js";
 import { add, create, parse } from "../utils/feed.js";
 import { CONTENT_TYPES, ERRORS, LIMIT } from "../utils/consts.js";
 
-const USER_NAME_REGEX = /^[0-9a-zA-Z_]+$/;
-
 const INIT = {
   headers: {
     authorization:
@@ -22,49 +20,46 @@ async function setupInit() {
 }
 
 async function user(ctx, next) {
+  const url = new URL(
+    "https://twitter.com/i/api/1.1/statuses/user_timeline.json",
+  );
+  url.searchParams.set("count", LIMIT);
+  url.searchParams.set("screen_name", ctx.params.user);
+
   let response;
-  if (ctx.params.user.match(USER_NAME_REGEX) !== null) {
-    try {
-      await setupInit();
+  try {
+    await setupInit();
 
-      response = await (await fetchX(
-        `https://twitter.com/i/api/1.1/statuses/user_timeline.json?screen_name=${ctx.params.user}`,
-        INIT,
-      )).json();
-    } catch (_) {
-      ctx.response.body = ERRORS["FAILED_UPSTREAM"];
-      ctx.response.status = 500;
-      return;
-    }
-
-    const feed = create(
-      `https://twitter.com/i/user/${response[0]["user"]["id_str"]}`,
-      `Twitter - ${response[0]["user"]["name"]}`,
-      response[0]["user"]["name"],
-      response[0]["user"]["profile_image_url_https"],
-    );
-
-    for (let i = 0; i < response.length && i < LIMIT; i++) {
-      const title = response[i]["text"].replace(/(?:\r\n|\n|\r)/gm, "");
-      add(
-        feed,
-        `https://twitter.com/i/status/${response[i]["id_str"]}`,
-        title,
-        new Date(response[i]["created_at"]).toISOString(),
-        response[i]["user"]["text"],
-        response[i]["text"],
-        title,
-      );
-    }
-
-    ctx.response.body = parse(feed);
-    ctx.response.headers.set("content-type", CONTENT_TYPES.ATOM);
-    return;
-  } else {
-    ctx.response.body = ERRORS["INVALID_REQUEST"];
-    ctx.response.status = 400;
+    response = await (await fetchX(url, INIT)).json();
+  } catch (_) {
+    ctx.response.body = ERRORS["FAILED_UPSTREAM"];
+    ctx.response.status = 500;
     return;
   }
+
+  const feed = create(
+    `https://twitter.com/i/user/${response[0]["user"]["id_str"]}`,
+    `Twitter - ${response[0]["user"]["name"]}`,
+    response[0]["user"]["name"],
+    response[0]["user"]["profile_image_url_https"],
+  );
+
+  for (let i = 0; i < response.length && i < LIMIT; i++) {
+    const title = response[i]["text"].replace(/(?:\r\n|\n|\r)/gm, "");
+    add(
+      feed,
+      `https://twitter.com/i/status/${response[i]["id_str"]}`,
+      title,
+      new Date(response[i]["created_at"]).toISOString(),
+      response[i]["user"]["text"],
+      response[i]["text"],
+      title,
+    );
+  }
+
+  ctx.response.body = parse(feed);
+  ctx.response.headers.set("content-type", CONTENT_TYPES.ATOM);
+  return;
 }
 
 export { user };
